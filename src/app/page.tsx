fa9449ae8574dd18a5487d0d1cc395db4014c5dd
umar mohammad sheikh <sumar@proficientnowtech.com>
2025-07-23 21:36:48 +0530

    feat(hero): add hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ import '@/lib/env';
 
 import CoreValues from '@/app/components/CoreValues';
 import Hero from '@/app/components/HeroSection';
+import ResearchSection from '@/app/components/ResearchSection';
+import TransformSection from '@/app/components/TransformSection';
+import TrustedBy from '@/app/components/TrustedBy';
 import Vision from '@/app/components/Vision';
+import WhyChooseUs from '@/app/components/WhyChooseUs';
 
 /**
  * SVGR Support
@@ -30,6 +34,10 @@ export default function HomePage() {
         <Hero />
         <CoreValues />
         <Vision />
+        <TrustedBy />
+        <TransformSection />
+        <WhyChooseUs />
+        <ResearchSection />
       </section>
     </main>
   );
